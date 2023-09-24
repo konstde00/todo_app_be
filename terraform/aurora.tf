833a0ac39bf8042fedc610bc08c52620c59218da
kdementiev <kostiantyn.dementiev@observepoint.com>
2023-09-24 22:50:53 +0300

    Fixed pipeline

--- terraform/aurora.tf
+++ terraform/aurora.tf
@@ -1,19 +1,42 @@
-resource "aws_rds_cluster" "example" {
-  cluster_identifier = "example"
+#resource "aws_rds_cluster" "todo_app_db_cluster" {
+#  cluster_identifier = "todo-app-db-cluster"
+#  engine                  = "mysql"
+#  engine_version          = "8.0.34"
+#  engine_mode        = "provisioned"
+#  database_name      = "todo_app_db"
+#  master_username    = "todo_app"
+#  master_password    = "must_be_eight_characters"
+#  availability_zones = [
+#    "us-east-1a",
+#    "us-east-1b",
+#    "us-east-1c"
+#  ]
+#  vpc_security_group_ids = [aws_security_group.aurora_sg.id]
+#  allocated_storage = 5
+#  db_cluster_instance_class = "db.t2.micro"
+#
+#  preferred_backup_window = "07:00-09:00"
+#  db_subnet_group_name    = aws_db_subnet_group.aurora_subnet_group.name
+#
+#  skip_final_snapshot = true
+#
+#  timeouts {
+#    create = "1h"
+#    update = "2h"
+#    delete = "1h"
+#  }
+#}
+
+resource "aws_rds_cluster" "todo_app_db_cluster" {
+  cluster_identifier = "todo-app-db-cluster"
   engine             = "aurora-mysql"
-  engine_version     = "5.7.mysql_aurora.2.03.2"
   engine_mode        = "provisioned"
   database_name      = "todo_app_db"
   master_username    = "todo_app"
   master_password    = "must_be_eight_characters"
-  availability_zones = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c"
-  ]
 
+  vpc_security_group_ids  = [aws_security_group.aurora_sg.id]
   preferred_backup_window = "07:00-09:00"
-  db_subnet_group_name    = aws_db_subnet_group.aurora_subnet_group.name
 
   serverlessv2_scaling_configuration {
     max_capacity = 1.0
@@ -21,6 +44,7 @@ resource "aws_rds_cluster" "example" {
   }
 
   skip_final_snapshot = true
+  db_subnet_group_name = aws_db_subnet_group.aurora_subnet_group.name
 
   timeouts {
     create = "1h"
@@ -29,15 +53,41 @@ resource "aws_rds_cluster" "example" {
   }
 }
 
-resource "aws_rds_cluster_instance" "example" {
-  cluster_identifier = aws_rds_cluster.example.id
+resource "aws_rds_cluster_instance" "todo_app_db_instance" {
+  cluster_identifier = aws_rds_cluster.todo_app_db_cluster.id
   instance_class     = "db.serverless"
-  engine             = aws_rds_cluster.example.engine
-  engine_version     = aws_rds_cluster.example.engine_version
+  engine             = aws_rds_cluster.todo_app_db_cluster.engine
+  engine_version     = aws_rds_cluster.todo_app_db_cluster.engine_version
 
   timeouts {
     create = "1h"
     update = "2h"
     delete = "1h"
+  }
+}
+
+resource "aws_security_group" "aurora_sg" {
+  name        = "aurora_sg"
+  description = "Security group for Aurora DB cluster"
+  vpc_id      = module.main_vpc.vpc_id
+
+  ingress {
+    description     = "TLS from VPC"
+    security_groups = [aws_security_group.api_sg.id]
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "aurora_sg"
   }
 }
--- terraform/env/todo_app_be.tfvars
+++ terraform/env/todo_app_be.tfvars
@@ -1,6 +1,6 @@
-environment_name            = "staging"
-spring_profile              = "staging"
-api_desired_count           = 1
+environment_name  = "staging"
+spring_profile    = "dev"
+api_desired_count = 1
 
 apigw_authorizer_id = ""
 
@@ -11,6 +11,6 @@ tags = {
 
 apigw_service_name = "todo_app_api"
 
-api_cpu          = 1024
-api_memory       = 2048
-api_java_heap_mb = 1500
+api_cpu          = 256
+api_memory       = 512
+api_java_heap_mb = 400
