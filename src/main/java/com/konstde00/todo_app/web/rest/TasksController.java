da88ed75297183109ca70263c2e84391d669db11
kdementiev <kostiantyn.dementiev@observepoint.com>
2023-10-31 16:45:13 +0200

    Added GoogleDrive integration

--- src/main/java/com/konstde00/todo_app/web/rest/TasksController.java
+++ src/main/java/com/konstde00/todo_app/web/rest/TasksController.java
@@ -2,6 +2,7 @@ package com.konstde00.todo_app.web.rest;
 
 import com.konstde00.todo_app.domain.Task;
 import com.konstde00.todo_app.domain.User;
+import com.konstde00.todo_app.service.GoogleDriveService;
 import com.konstde00.todo_app.service.TaskService;
 import com.konstde00.todo_app.service.UserService;
 import com.konstde00.todo_app.service.api.dto.*;
@@ -12,7 +13,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -22,6 +26,7 @@ public class TasksController implements TasksApi {
 
   TaskService taskService;
   UserService userService;
+  GoogleDriveService googleDriveService;
 
   @Override
   public ResponseEntity<GetTasksResponseDto> getTasks(
@@ -60,6 +65,13 @@ public class TasksController implements TasksApi {
     UpdateTaskResponseDto updatedTask = taskService.updateTask(taskId, updateTaskRequestDto);
 
     return ResponseEntity.ok(updatedTask);
+  }
+
+  @PostMapping("/api/tasks/v1/upload")
+  public ResponseEntity<String> uploadFileToGoogleDrive(@RequestParam Long taskId,
+                                                        @RequestParam MultipartFile file) {
+
+    return ResponseEntity.ok(googleDriveService.uploadFile(taskId, file));
   }
 
   @Override
